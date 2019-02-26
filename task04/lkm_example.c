c374c5381802e65e3fcadd8f10e58c28c2f74f22
Katherine Perez <perezkh@gmail.com>
2019-02-26 03:10:26 +0000

    correct style in lkm_example.c

--- task04/lkm_example.c
+++ task04/lkm_example.c
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+/*
+ * Example driver for the Eudyptula Challenge
+ *
+ * Copyright (C) 2019 Katherine Perez <perezkh@gmail.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -7,12 +19,14 @@ MODULE_AUTHOR("Katherine Perez");
 MODULE_DESCRIPTION("A simple example Linux module.");
 MODULE_VERSION("0.01");
 
-static int __init lkm_example_init(void) {
- printk(KERN_INFO "Hello, My Neighbor!\n");
- return 0;
+static int __init lkm_example_init(void)
+{
+	netdev_info(netdevdev "Hello, My Neighbor!\n");
+	return 0;
 }
-static void __exit lkm_example_exit(void) {
- printk(KERN_INFO "Goodbye, My Neighbor!\n");
+static void __exit lkm_example_exit(void)
+{
+	netdev_info(netdevdev "Goodbye, My Neighbor!\n");
 }
 
 module_init(lkm_example_init);
